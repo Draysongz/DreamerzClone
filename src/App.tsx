99e4246cf184c6bec144e13fc9fb9452efe24a53
Levi <70198955+Draysongz@users.noreply.github.com>
2024-10-06 08:36:55 +0100

    fdone

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [telegramInitData, setTelegramInitData] = useState<string>("");
+  const [photoUrl, setPhotoUrl] = useState<string>("")
 
   const { login, user } = useAuth();
   // const telegramInitData ="query_id=AAElBO5_AAAAACUE7n-OXKaOuser=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1728157663&hash=88abaf102c4ab57a7e55922b2cd25debd5361e81becf43e10e3625225a2c6345"
@@ -22,6 +23,10 @@ function App() {
   // Effect to set Telegram init data
   useEffect(() => {
     const initData = WebApp.initData;
+    const photo_url = WebApp.initDataUnsafe.user?.photo_url
+    if(photo_url){
+      setPhotoUrl(photo_url)
+    }
     setTelegramInitData(initData);
   }, []);
 
@@ -33,8 +38,8 @@ function App() {
 
   // Call the login function when telegramInitData is available
   useEffect(() => {
-    if (telegramInitData) {
-      login(telegramInitData);
+    if (telegramInitData && photoUrl) {
+      login(telegramInitData, photoUrl);
     }
   }, [telegramInitData, login]);
 
